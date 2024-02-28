6cb637735872ed07cdf96ffd65edb9b45098e502
Colin T.A. Gray <colinta@gmail.com>
2024-02-27 21:09:10 -0500

    redirects!

---
+++ src/functions/getRedirects.js
@@ -0,0 +1,30 @@
+import { loadEnv } from "vite";
+import StoryblokClient from 'storyblok-js-client'
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
+export async function getRedirects() {
+  const storyblokApi = new StoryblokClient({
+    region: "us",
+    accessToken: env.STORYBLOK_TOKEN,
+    cache: {
+      clear: 'auto',
+      type: 'memory',
+    },
+  })
+
+  const { data } = await storyblokApi.get('cdn/stories/redirects', {
+    version: 'draft',
+    resolve_relations: 'redirect_entry.destination',
+  })
+
+  const storyblockRedirects = data.story.content.redirect_entries
+  const astroRedirects = {}
+  for (const redirect of storyblockRedirects) {
+    const targetUrl = redirect.destination.full_slug === 'home' ? '/' : `/${redirect.destination.full_slug}`
+    astroRedirects[redirect.source_url] = targetUrl
+  }
+
+  console.log(`Found #${Object.keys(astroRedirects).length} redirects`);
+  return astroRedirects
+}

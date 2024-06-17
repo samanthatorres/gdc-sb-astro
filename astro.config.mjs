2518934ade9de44c478c10d0f73f68e69755579f
Colin T.A. Gray <colinta@gmail.com>
2024-06-17 14:47:24 -0400

    Revert "trailing slash config"
    This reverts commit 90aef4c61d9ac5ce4c29ef7d640c13266885c75f.

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,6 @@ const redirects = await getRedirects();
 // https://astro.build/config
 export default defineConfig({
   redirects,
-  trailingSlash: "never",
   integrations: [
     storyblok({
       accessToken: env.STORYBLOK_TOKEN,

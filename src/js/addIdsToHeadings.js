891a5ce62e5ffebacac693426adb5d6b61330768
Colin T.A. Gray <colinta@gmail.com>
2025-02-18 21:14:33 -0500

    implement Wysiwyg component (and removes RichText - unused)

--- src/js/addIdsToHeadings.js
+++ src/js/addIdsToHeadings.js
@@ -1,26 +1,36 @@
-import { JSDOM } from 'jsdom';
+import {JSDOM} from 'jsdom';
+
 export function generateSlug(text) {
-    return text.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/^-|-$/g, '');
+    return text
+        .toLowerCase()
+        .replace(/[^a-z0-9]+/g, '-')
+        .replace(/^-|-$/g, '');
+}
+
+export function generateSlugFromContent(content) {
+    const text = content.map(elem => elem.text).join('');
+    return text
+        .toLowerCase()
+        .replace(/[^a-z0-9]+/g, '-')
+        .replace(/^-|-$/g, '');
 }
 
 export function addIdsToHeadings(htmlString) {
-    
-    
     // Create a JSDOM instance from the HTML string
     const dom = new JSDOM(htmlString);
-    
+
     // Get the document object
     const doc = dom.window.document;
-    
+
     // Get all heading elements
     const headings = doc.querySelectorAll('h1, h2, h3');
-    
+
     // Iterate over each heading and add an id attribute
     headings.forEach(heading => {
         const slug = generateSlug(heading.textContent);
         heading.setAttribute('id', slug);
     });
-    
+
     // Serialize the modified HTML and return it
     return dom.serialize();
 }

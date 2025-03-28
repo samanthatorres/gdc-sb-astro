9d4c6041c0fe0968305d2fffaf7d4d62395930a0
samanthatorres <samscaldwell@gmail.com>
2025-03-28 14:02:59 -0400

    robots.txt update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import { getRedirects } from "./src/functions/getRedirects";
 import { defineConfig } from 'astro/config';
 import vercelStatic from '@astrojs/vercel/static';
 
+import robotsTxt from "astro-robots-txt";
+
 const env = loadEnv("", process.cwd(), ["STORYBLOK", "PUBLIC"]);
 const isDevelopment = process.env.NODE_ENV === 'development' || process.argv.includes('dev');
 const redirects = await getRedirects();
@@ -13,42 +15,40 @@ const redirects = await getRedirects();
 export default defineConfig({
   site: isDevelopment ? 'https://dev.thegray.company' : 'https://thegray.company',
   redirects,
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      apiOptions: {
-        region: "us",
-      },
-      bridge: {
-        resolveLinks: "url",
-      },
-      components: {
-        author: "storyblok/Author",
-        blog: "storyblok/Blog",
-        bloglist: "storyblok/BlogList",
-        carousel: "storyblok/Carousel",
-        childhero: "storyblok/ChildHero",
-        config: "storyblok/Config",
-        featured_blogs: "storyblok/FeaturedBlogs",
-        fourup: "storyblok/FourUp",
-        hero: "storyblok/Hero",
-        iconcard: "storyblok/IconCard",
-        image: "storyblok/Image",
-        leftright: "storyblok/LeftRight",
-        listitem: "storyblok/Listitem",
-        liststack: "storyblok/Liststack",
-        page: "storyblok/Page",
-        redirects_config: "storyblok/Redirect",
-        sidebyside: "storyblok/SideBySide",
-        testimonial: "storyblok/Testimonial",
-        testimonials: "storyblok/Testimonials",
-        textblock: "storyblok/Textblock",
-        threecolumn: "storyblok/ThreeColumn",
-        video: "storyblok/Video",
-        wysiwyg: "storyblok/Wysiwyg",
-      }
-    }),
-  ],
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    apiOptions: {
+      region: "us",
+    },
+    bridge: {
+      resolveLinks: "url",
+    },
+    components: {
+      author: "storyblok/Author",
+      blog: "storyblok/Blog",
+      bloglist: "storyblok/BlogList",
+      carousel: "storyblok/Carousel",
+      childhero: "storyblok/ChildHero",
+      config: "storyblok/Config",
+      featured_blogs: "storyblok/FeaturedBlogs",
+      fourup: "storyblok/FourUp",
+      hero: "storyblok/Hero",
+      iconcard: "storyblok/IconCard",
+      image: "storyblok/Image",
+      leftright: "storyblok/LeftRight",
+      listitem: "storyblok/Listitem",
+      liststack: "storyblok/Liststack",
+      page: "storyblok/Page",
+      redirects_config: "storyblok/Redirect",
+      sidebyside: "storyblok/SideBySide",
+      testimonial: "storyblok/Testimonial",
+      testimonials: "storyblok/Testimonials",
+      textblock: "storyblok/Textblock",
+      threecolumn: "storyblok/ThreeColumn",
+      video: "storyblok/Video",
+      wysiwyg: "storyblok/Wysiwyg",
+    }
+  }), robotsTxt()],
   vite: {
     optimizeDeps: { exclude: ["fsevents"] },
     plugins: [basicSsl()],
